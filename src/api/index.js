ada00ab8ba0a9e1ffa160ad81e86eb9552651aa7
Jamie Murray <jm1077@sky.com>
2021-09-23 12:01:45 +0100

    styles and functions for second list

--- src/api/index.js
+++ src/api/index.js
@@ -9,20 +9,56 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  let vehicles = await fetch("api/vehicles.json")
+  const vehicles = await fetch("api/vehicles.json")
     .then((res) => res.json())
     .then(data => {
       return data;
     })
     .catch(error => console.log('Looks like there was a problem!', error))
 
+  //Temporary media query to window resize
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+
+
+  //Remove reference to the 'problematic' data in the json and return a new vehicle list
+  const newVehicleList = vehicles.filter(function (vehicle) {
+    return vehicle.id !== "problematic";
+  });
+
+  const vehicleListJson = newVehicleList.map(vehicle => {
+    const apiUrls = vehicle.apiUrl;
+    return apiUrls;
+  })
+
+ const consolidatedList = Promise.all(vehicleListJson.map(url => fetch(url)
+ .then(response => response.json())
+ .then(data => {
+   return {...newVehicleList, ...data}
+ })
+ .catch(error => console.log('Looks like there was a problem!', error))
+ ))
+ 
+
+
+
   return(
-    vehicles.map(vehicle => 
-      <div key={vehicle.id}>
-        <h1>{vehicle.id}</h1>
-        <h2>From</h2>
-        <h3>Descripton</h3>
-      </div>
+    newVehicleList.map(vehicle => 
+      {
+        return(        
+          <div key={vehicle.id} className="vehicle">
+            <div className="vehicle__img">
+              <img src={mediaQuery.matches ? vehicle.media[1].url : vehicle.media[0].url} />
+            </div>
+
+            <div className="vehicle__info">
+              <h1>Jaguar {vehicle.id}</h1>
+              <h2>From £76,350</h2>
+              <h3>The pinnacle of refined capability</h3>
+            </div>
+
+          </div>
+        );
+      }
     )
   );
 }
